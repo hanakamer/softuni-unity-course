2de04d924884fb07ac5a871e445196604ebc8db7
Hanay Can <hanaycan@Hanays-MacBook-Pro.local>
2017-07-12 13:18:46 +0300

    add homeworks

---
+++ homeworks/homework-1/demo of level/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+	public GameObject target;
+	public GameObject enemy1;
+	public GameObject enemy2;
+	public GameObject enemy3;
+	public GameObject enemy4;
+	public GameObject enemy5;
+
+	public int cur;
+
+
+	public float flow_speed = 0.02F;
+	private Quaternion oldRot;
+	private Quaternion newRot;
+	void Start () {
+		cur = GameObject.Find("capsule").GetComponent<CapsuleScript>().current;
+
+	}
+	void Update () {
+		cur = GameObject.Find("capsule").GetComponent<CapsuleScript>().current;
+		if(cur<4){
+			RotateSlowly (enemy1);
+		} else if(cur>3 && cur<5){
+			RotateSlowly (enemy2);
+		} else if(cur>4 && cur<8){
+			RotateSlowly (enemy3);
+		} else if(cur>8 && cur<10){
+			RotateSlowly (enemy4);
+		} else if(cur>9){
+			RotateSlowly (enemy5);
+		} 
+
+	}
+
+	void RotateSlowly(GameObject enemy){
+		oldRot   = transform.rotation ;
+		transform.LookAt ( enemy.transform ) ;
+		newRot   = transform.rotation ;
+		transform.rotation = Quaternion.Lerp ( oldRot , newRot, flow_speed ) ;
+	}
+}
---
+++ homeworks/homework-1/demo of level/Assets/Scripts/CapsuleScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleScript : MonoBehaviour {
+	public Transform[] target;
+	public float speed;
+
+	public int current;
+
+	void Start () {
+		
+	}
+		void Update () {
+		Debug.Log (transform.position +" "+ target[current].position);
+
+		if (transform.position != target [current].position) {
+			Vector3 pos = Vector3.MoveTowards (transform.position, target [current].position, speed * Time.deltaTime);
+			GetComponent<Rigidbody> ().MovePosition (pos);
+
+		} else {
+			if (current < 10) {
+				current = (current + 1);
+			} else {
+				speed =  0;
+			}
+		}
+		
+		
+	}
+}
